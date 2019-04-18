a7a92e988d44b87ef904ad16647872b6790aade5
ElmerzGlue <jem8760@gmail.com>
2019-04-18 14:39:50 -0500

    Fix tests, update Home title

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class StaticControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "| Show Me Mock Trial"
+    @base_title = "Show Me Mock Trial"
   end
 
   test "should get home" do
-    get static_home_url
+    get home_path
     assert_response :success
-    assert_select "title", "Home #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get about" do
-    get static_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About #{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Contact #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
